f4830382363f915c001bc4e9fd257b9b328735ac
Andrey <vedischev76@gmail.com>
2022-09-03 18:05:19 +0500

    update task3

--- task3.py
+++ task3.py
@@ -7,5 +7,14 @@
 #     - x=2; y=4-> 1
 #     - x=-34; y=-30 -> 3
 
-x = int(input('Введите координаты x: '))
-y = int(input('Введите координаты y: '))
+x = int(input('Введите координаты X: '))
+y = int(input('Введите координаты Y: '))
+
+if (x > 0 and y > 0):
+    print(f'{x} {y} --> 1')
+elif (x < 0 and y > 0):
+    print(f'{x} {y} --> 2')
+elif (x < 0 and y < 0):
+    print(f'{x} {y} --> 3')
+elif (x > 0 and y < 0):
+    print(f'{x} {y} --> 4')
