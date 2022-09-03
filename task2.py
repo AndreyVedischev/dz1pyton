e31f89dd0777b23e7b73a874eaa11363c75ab418
Andrey <vedischev76@gmail.com>
2022-09-03 17:51:49 +0500

    update task2

--- task2.py
+++ task2.py
@@ -1,2 +1,13 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
+from random import randint
+
+x = randint(0,1)
+y = randint(0,1)
+z = randint(0,1)
+print(x,y,z)
+
+if (not(x or y or z)) == (not x and not y and not z):
+    print('Истинно')
+else:
+    print('Ложно')
